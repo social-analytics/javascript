055f24411af480563a2fd6a4ec22585f6c738c5b
samhuntsocial <samson.poon@hunt.social>
2015-07-22 16:44:39 +0800

    test.js
    wfwefwef

---
+++ test.js
@@ -0,0 +1,87 @@
+// for the feeds/
+var data = [
+    {
+        panelIndex: 0, 
+        pageIndex: 1,
+        page:
+        [
+            {
+                titleOrPicUrl: "title1",
+                url: "http://www.nba.com",
+                sourceType: "Q&A", 
+                content: "content1", 
+                sourceDomain: "nba.com",
+                pDate: "2 days ago"
+                
+            },
+            {
+                content: "1content", 
+                titleOrPicUrl: "1title",
+                url: "http://www.nba.com" ,
+                sourceType: "Q&A", 
+                sourceDomain: "nba.com",
+                pDate: "2 days ago"
+                
+            },
+            {
+                content: "content11", 
+                titleOrPicUrl: "title11",
+                url: "http://www.nba.com" ,
+                sourceType: "Q&A", 
+                sourceDomain: "nba.com",
+                pDate: "2 days ago"
+                
+            }                    
+        ]
+    }, 
+    {
+        panelIndex: 1, 
+        pageIndex: 2,
+        page:
+        [
+            {
+                content: "content2", 
+                titleOrPicUrl: "title2",
+                url: "http://www.nba.com",
+                sourceType: "Q&A", 
+                sourceDomain: "nba.com",
+                pDate: "2 days ago"
+                
+            },
+            {
+                content: "2content", 
+                titleOrPicUrl: "2title",
+                url: "http://www.nba.com"  ,
+                sourceType: "Q&A", 
+                sourceDomain: "nba.com",
+                pDate: "2 days ago"
+                
+            }
+        ]
+    }, 
+    {
+        panelIndex: 2,
+        pageIndex: 3,
+        page:
+        [
+            {
+                content: "content3", 
+                titleOrPicUrl: "title3",
+                url: "http://www.nba.com",
+                sourceType: "Q&A", 
+                sourceDomain: "nba.com",
+                pDate: "2 days ago"
+                
+            },
+            {
+                content: "3content", 
+                titleOrPicUrl: "3title",
+                url: "http://www.nba.com" ,
+                sourceType: "Q&A", 
+                sourceDomain: "nba.com",
+                pDate: "2 days ago"
+                
+            }
+        ]
+    }
+];
